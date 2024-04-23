330df43967064bc9c67d4daf29c61ea68dbdeb22
Harsat <mimkal@mail.ru>
2024-04-24 00:08:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobilelab2_6/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.mobilelab2_6
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var navigationView: NavigationView
+
+    lateinit var listener: NavController.OnDestinationChangedListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        navController = findNavController(R.id.fragment)
+        drawerLayout = findViewById(R.id.main)
+        navigationView = findViewById(R.id.navigationView)
+        navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration)
+                ||  super.onSupportNavigateUp()
+    }
+}
